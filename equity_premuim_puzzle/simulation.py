6e45642c43bb7647e54e06d5a1f679d305b09e2e
Oliver Wilkins <wilkins@oxy.edu>
2024-09-17 15:24:49 -0700

    Added functionality to get utility of choosing to invest in stocks

--- equity_premuim_puzzle/simulation.py
+++ equity_premuim_puzzle/simulation.py
@@ -12,30 +12,21 @@ import itertools
 
 def utility(wealth):
     A = 2
-    return 10000 * (wealth ** (1-A)) / (1-A)
+    return 10000 * (wealth ** float((1-A))) / (1-A)
 
-def get_exp_utility(starting_wealth, bin_values, bin_counts, num_repetitions):
-        # Normalize bin_counts to get probabilities
+def get_exp_utility(starting_wealth, bin_values, bin_counts, num_repetitions, num_samples=100000):
     probabilities = bin_counts / np.sum(bin_counts)
-
-        # Generate all possible combinations of bin_values over num_repetitions
-    all_combinations = list(itertools.product(bin_values, repeat=num_repetitions))
-
     expected_utility = 0
 
-        # Iterate over each combination
-    for combination in all_combinations:
-            # Calculate the probability of this combination (product of individual probabilities)
-        combination_prob = np.prod([probabilities[bin_values.index(value)] for value in combination])
-
-            # Calculate the resulting wealth for this combination
-        resulting_wealth = starting_wealth * np.prod(combination)
-
-            # Apply the utility function to the resulting wealth
+    # Use Monte Carlo sampling to approximate the expected utility
+    for _ in range(num_samples):
+        sampled_combination = np.random.choice(bin_values, size=num_repetitions, p=probabilities)
+        resulting_wealth = starting_wealth * np.prod(sampled_combination)
         util = utility(resulting_wealth)
-
-            # Add the weighted utility to the expected utility
+        combination_prob = np.prod([probabilities[bin_values.index(value)] for value in sampled_combination])
         expected_utility += combination_prob * util
+    expected_utility /= num_samples
+
     return expected_utility
 
 def generate_bins(num_bins):
@@ -59,12 +50,19 @@ def generate_bins(num_bins):
     #     print ('bin avg: ' + str(values[i]) + ', bin count: ' + str(counts[i]))
     return(values, counts)
 
+def get_total_bond_return(num_months):
+    df = pd.read_csv('Bond_Yield.csv')
+    annual_returns = df['DGS10'].values
+    median_return = np.mean(annual_returns)
+    return (1 + (median_return * .01)) ** (num_months/12)
+
 
 starting_wealth = 100
+num_months = 120
 num_bins = 10
-# (bin_values, bin_counts) = generate_bins(num_bins)
+(bin_values, bin_counts) = generate_bins(num_bins)
 # for i in range(0, num_bins):
 #         print ('bin avg: ' + str(bin_values[i]) + ', bin count: ' + str(bin_counts[i]))
-bin_values = [-1, 1, 2]
-bin_counts = [1, 2, 1]
-print("starting utility: " + str(utility(starting_wealth)) + "\nexpexted ending utility: " + str(get_exp_utility(starting_wealth, bin_values, bin_counts, 10)))
+# bin_values = [-1, 1, 2]
+# bin_counts = [1, 2, 1]
+print("Bond utility: " + str(utility(get_total_bond_return(num_months) * starting_wealth)) + "\nexpexted stock utility: " + str(get_exp_utility(starting_wealth, bin_values, bin_counts, 10)))
