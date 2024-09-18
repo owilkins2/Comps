b7150829f6bdd19d2d64aef68c310613282092b6
Oliver Wilkins <wilkins@oxy.edu>
2024-09-18 15:31:29 -0700

    Simulation is now complete and calibrated for standard theory

--- equity_premuim_puzzle/simulation.py
+++ equity_premuim_puzzle/simulation.py
@@ -1,18 +1,20 @@
-#For standard theory: utility function is fixed, find the minimum E.P. such that investment is chosen, i.e. such that starting utility = exp_utility
+# For standard theory: utility function is fixed, find the minimum E.P. such that investment is chosen, i.e. such that starting utility = exp_utility
 #                   **(manipulate E.P. by chaning mean of distribution, other attributes the same)
-#For behavioral considerations: utility function is variable w/ bracketing and loss aversion, E.P. is fixed by emperical data
+# For behavioral considerations: utility function is variable w/ bracketing and loss aversion, E.P. is fixed by emperical data
 
 
 import math
 import seaborn as sns
 import pandas as pd
-import matplotlib
+import matplotlib.pyplot as plt
 import numpy as np
 import itertools
 
+
 def utility(wealth):
     A = 2
-    return 10000 * (wealth ** float((1-A))) / (1-A)
+    return 10000 * (wealth ** float((1 - A))) / (1 - A)
+
 
 def get_exp_utility(starting_wealth, bin_values, bin_counts, num_repetitions, num_samples=100000):
     probabilities = bin_counts / np.sum(bin_counts)
@@ -23,46 +25,71 @@ def get_exp_utility(starting_wealth, bin_values, bin_counts, num_repetitions, nu
         sampled_combination = np.random.choice(bin_values, size=num_repetitions, p=probabilities)
         resulting_wealth = starting_wealth * np.prod(sampled_combination)
         util = utility(resulting_wealth)
-        combination_prob = np.prod([probabilities[bin_values.index(value)] for value in sampled_combination])
-        expected_utility += combination_prob * util
+        expected_utility += util
     expected_utility /= num_samples
 
     return expected_utility
+
 
 def generate_bins(num_bins):
     df = pd.read_csv('SP500_percent_changes.csv')
     percent_changes = df['SP500_PCH'].values
     returns = [1 + (.01 * percent_change) for percent_change in percent_changes]
+
     min = np.min(returns)
     max = np.max(returns)
-    spread = max-min
-    bin_width = spread/num_bins
+    spread = max - min
+    bin_width = spread / num_bins
     values = []
     counts = []
     for i in range(0, num_bins):
         values.append(min + (.5 * bin_width) + (i * bin_width))
         count = 0
         for value in returns:
-            if ( (value >= min + (i * bin_width)) and (value < min + ((i +1) * bin_width)) ):
+            if ((value >= min + (i * bin_width)) and (value < min + ((i + 1) * bin_width))):
                 count += 1
         counts.append(count)
     # for i in range(0, num_bins):
     #     print ('bin avg: ' + str(values[i]) + ', bin count: ' + str(counts[i]))
-    return(values, counts)
+    return (values, counts)
 
-def get_total_bond_return(num_months):
+
+def get_bond_mean():
     df = pd.read_csv('Bond_Yield.csv')
     annual_returns = df['DGS10'].values
-    median_return = np.mean(annual_returns)
-    return (1 + (median_return * .01)) ** (num_months/12)
+    return np.mean(annual_returns)
+
+
+def get_total_bond_return(num_months):
+    return (1 + (get_bond_mean() * .01)) ** (num_months / 12)
+
+
+def get_distribution_mean(bin_values, bin_counts):
+    sum = 0
+    count = 0
+    for i in range(len(bin_values)):
+        sum += bin_values[i] * bin_counts[i]
+        count += bin_counts[i]
+    return sum / count
+
+
+def shift_distribution_mean(bin_values, annualized_amount):
+    shifted_bin_values = [value + (1/12 * annualized_amount) for value in bin_values]
+    return shifted_bin_values
 
 
 starting_wealth = 100
 num_months = 120
-num_bins = 10
+num_bins = 20
 (bin_values, bin_counts) = generate_bins(num_bins)
-# for i in range(0, num_bins):
-#         print ('bin avg: ' + str(bin_values[i]) + ', bin count: ' + str(bin_counts[i]))
+new_bin_values = shift_distribution_mean(bin_values, -.0265)
+for i in range(0, num_bins):
+         print ('bin avg: ' + str(new_bin_values[i]) + ', bin count: ' + str(bin_counts[i]))
 # bin_values = [-1, 1, 2]
 # bin_counts = [1, 2, 1]
-print("Bond utility: " + str(utility(get_total_bond_return(num_months) * starting_wealth)) + "\nexpexted stock utility: " + str(get_exp_utility(starting_wealth, bin_values, bin_counts, 10)))
+print("Equity Premium: " + str((((get_distribution_mean(new_bin_values, bin_counts)** 12) -1) * 100) - get_bond_mean()))
+print("Utility Difference: " + str(get_exp_utility(starting_wealth, new_bin_values, bin_counts, num_months) -
+    utility(get_total_bond_return(num_months) * starting_wealth)))
+print("Stock Utility: " + str(get_exp_utility(starting_wealth, new_bin_values, bin_counts, num_months)) + ", Bond Utility: " +
+    str(utility(get_total_bond_return(num_months) * starting_wealth)))
+    
