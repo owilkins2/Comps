ddab81618ca84bbc437d4c4ee29b0625eca2294e
Oliver Wilkins <wilkins@oxy.edu>
2024-09-27 12:35:43 -0700

    Fixed logical error in the behavioral utility calcultion. Added function to create a heatmap of possible combinations of loss aversion and bracket size.

--- equity_premuim_puzzle/simulation.py
+++ equity_premuim_puzzle/simulation.py
@@ -16,6 +16,13 @@ def utility(wealth):
     return 10000 * (wealth ** float((1 - A))) / (1 - A)
 
 
+def gain_loss_utility(change_in_wealth, loss_aversion_coefficient):
+    if change_in_wealth >= 0:
+        return change_in_wealth
+    else:
+        return change_in_wealth * loss_aversion_coefficient
+
+
 def get_exp_utility(starting_wealth, bin_values, bin_counts, num_repetitions, num_samples=100000):
     probabilities = bin_counts / np.sum(bin_counts)
     expected_utility = 0
@@ -26,36 +33,6 @@ def get_exp_utility(starting_wealth, bin_values, bin_counts, num_repetitions, nu
         resulting_wealth = starting_wealth * np.prod(sampled_combination)
         util = utility(resulting_wealth)
         expected_utility += util
-    expected_utility /= num_samples
-
-    return expected_utility
-
-def get_behavioral_utility(starting_wealth, bin_values, bin_counts, num_repetitions, bracket_size, loss_aversion_coefficient, num_samples=10000):
-    probabilities = bin_counts / np.sum(bin_counts)
-    expected_utility = 0
-
-    for _ in range(num_samples):
-        index = 0
-        while(index < num_repetitions):
-            sampled_combination = None
-            full_bracket= True
-            if(num_repetitions - index) > bracket_size:
-                sampled_combination = np.random.choice(bin_values, size=bracket_size, p=probabilities)
-            else:
-                sampled_combination = np.random.choice(bin_values, size=num_repetitions - index, p=probabilities)
-                full_bracket = False
-            resulting_wealth = starting_wealth * np.prod(sampled_combination)
-            util = 0
-            if (resulting_wealth >= starting_wealth):
-                util = utility(resulting_wealth)
-            else:
-                loss = starting_wealth - resulting_wealth
-                util = utility(starting_wealth - (loss * loss_aversion_coefficient))
-            if full_bracket:
-                expected_utility += util * (bracket_size / num_repetitions)
-            else:
-                expected_utility += util * ((num_repetitions - index) / num_repetitions)
-            index += bracket_size
     expected_utility /= num_samples
 
     return expected_utility
@@ -104,8 +81,52 @@ def get_distribution_mean(bin_values, bin_counts):
 
 
 def shift_distribution_mean(bin_values, annualized_amount):
-    shifted_bin_values = [value + (1/12 * annualized_amount) for value in bin_values]
+    shifted_bin_values = [value + (1 / 12 * annualized_amount) for value in bin_values]
     return shifted_bin_values
+
+
+def get_behavioral_utility_differential(starting_wealth, bin_values, bin_counts, num_repetitions, bracket_size,
+                                        loss_aversion_coefficient, num_samples=10000):
+    probabilities = bin_counts / np.sum(bin_counts)
+    utility_differential = 0
+    monthly_bond_return = (1 + (.01 * get_bond_mean())) ** (1 / 12)
+
+    for _ in range(num_samples):
+        index = 0
+        utility_differential_local = 0
+
+        while (index < num_repetitions):
+            sampled_combination = None
+            full_bracket = True
+            if (num_repetitions - index) > bracket_size:
+                sampled_combination = np.random.choice(bin_values, size=bracket_size, p=probabilities)
+            else:
+                sampled_combination = np.random.choice(bin_values, size=num_repetitions - index, p=probabilities)
+                full_bracket = False
+            resulting_wealth_stock = starting_wealth * np.prod(sampled_combination)
+            resulting_wealth_bond = starting_wealth * (monthly_bond_return ** bracket_size)
+            util_bond = utility(resulting_wealth_bond)
+            util_stock = 0
+
+            if (resulting_wealth_stock >= starting_wealth):
+                util_stock = utility(resulting_wealth_stock)
+            else:
+                loss = starting_wealth - resulting_wealth_stock
+                util_stock = utility(starting_wealth - (loss * loss_aversion_coefficient))
+
+            utility_differential_local += (util_stock - util_bond)
+            # print("Resulting Wealth S: " + str(resulting_wealth_stock) + ", Resulting Wealth B: " + str(resulting_wealth_bond)
+            #       + ", Utility D: " + str(utility_differential))
+
+            # if full_bracket:
+            #     utility_differential += (abs(util_stock) - abs(util_bond)) * (bracket_size / num_repetitions)
+            # else:
+            #     utility_differential += (util_stock - util_bond) * ((num_repetitions - index) / num_repetitions)
+            index += bracket_size
+        utility_differential += utility_differential_local
+    utility_differential /= num_samples
+
+    return utility_differential
 
 
 def standard_calibration():
@@ -116,14 +137,20 @@ def standard_calibration():
     shift_amount = -.1
     equity_premiums = []
     utility_differentials = []
-    while(shift_amount <= 0):
+    while shift_amount <= 0:
         new_bin_values = shift_distribution_mean(bin_values, shift_amount)
-        equity_premiums.append((((get_distribution_mean(new_bin_values, bin_counts)** 12) -1) * 100) - get_bond_mean())
+        equity_premiums.append(
+            (((get_distribution_mean(new_bin_values, bin_counts) ** 12) - 1) * 100) - get_bond_mean())
         utility_differentials.append(get_exp_utility(starting_wealth, new_bin_values, bin_counts, num_months, 1000)
-            - utility(get_total_bond_return(num_months) * starting_wealth))
+                                     - utility(get_total_bond_return(num_months) * starting_wealth))
         shift_amount += .001
     plt.scatter(equity_premiums, utility_differentials)
+    plt.xlabel("Equity Premium (%)")
+    plt.ylabel("Utility Differential (Stocks - Bonds)")
+    plt.axline((0, 0), slope=0)
+    plt.savefig("Equity Premiums Graph")
     plt.show()
+
 
 def behavioral_calibration():
     starting_wealth = 100
@@ -133,13 +160,47 @@ def behavioral_calibration():
     loss_aversion_coefficient = 2
     (bin_values, bin_counts) = generate_bins(num_bins)
     bracket_sizes = []
-    utilities = []
+    utility_differentials = []
     while bracket_size <= num_months:
         bracket_sizes.append(bracket_size)
-        utilities.append(get_behavioral_utility(starting_wealth, bin_values, bin_counts, num_months, bracket_size, loss_aversion_coefficient, 10))
+        utility_differentials.append(
+            get_behavioral_utility_differential(starting_wealth, bin_values, bin_counts, num_months, bracket_size,
+                                                loss_aversion_coefficient, 1000))
         bracket_size += 1
-    print("Bond Utility: " + str(utility(get_total_bond_return(num_months) * starting_wealth)))
-    plt.scatter(bracket_sizes, utilities)
+    bond_utility = utility(get_total_bond_return(num_months) * starting_wealth)
+    print("Bond Utility: " + str(bond_utility))
+    plt.scatter(bracket_sizes, utility_differentials)
+    plt.xlabel("Bracket Size (months)")
+    plt.ylabel("Utility Differential")
+    plt.savefig("Bracket Sizes Graph")
     plt.show()
 
-standard_calibration()
+
+def create_heatmap():
+    data = []
+    (bin_values, bin_counts) = generate_bins(20)
+    for loss_aversion_x10 in range(10, 40, 1):
+        loss_aversion = loss_aversion_x10 / 10
+        for bracketing in range(1, 50, 1):
+            utility_differential = get_behavioral_utility_differential(100, bin_values, bin_counts, 120, bracketing,
+                                                                       loss_aversion, 100000)
+            data.append([loss_aversion, bracketing, abs(utility_differential)])
+
+    df = pd.DataFrame(data, columns=['Loss Aversion', 'Bracket Size', 'Utility Differential'])
+
+    pivot_table = df.pivot(index='Loss Aversion', columns='Bracket Size', values='Utility Differential')
+
+    plt.figure(figsize=(8, 6))
+    sns.heatmap(pivot_table, annot=False, cmap="Blues_r", cbar=True, vmax=100, vmin=0)
+
+    plt.title('Utility Differential Heatmap')
+    plt.xlabel('Bracket Size (months)')
+    plt.ylabel('Loss Aversion Coefficient')
+
+    plt.savefig("heatmap")
+    plt.show()
+
+
+create_heatmap()
+# standard_calibration()
+# behavioral_calibration()
